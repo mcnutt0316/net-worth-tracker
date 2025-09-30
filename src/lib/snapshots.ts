2d6d69e418b5bdcb522bfbd03574fcfe313fba28
Corey McNutt <mcnutt0316@gmail.com>
2025-09-30 11:32:32 -0500

    added utility function to snapshots.ts

--- src/lib/snapshots.ts
+++ src/lib/snapshots.ts
@@ -7,3 +7,43 @@ const globalForPrisma = globalThis as unknown as {
 const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export type CreateSnapshotData = {
+    assets: number
+    liabilities: number
+    networth: number
+}
+
+export async function createSnapshot(userId: string, data: CreateSnapshotData){
+    try{
+        const netWorth = await prisma.netWorthSnapshot.create({
+            data: {
+                networth: data.networth,
+                assets: data.assets,
+                liabilities: data.liabilities,
+                userId: userId
+            }
+        })
+        return netWorth
+    } catch (error) {
+        console.error('Error creating snapshot:', error)
+        throw new Error('Failed to create snapshot')
+    }
+}
+
+export async function getSnapshots(userId: string){
+    try {
+        const netWorth = await prisma.netWorthSnapshot.findMany({
+            where: {
+                userId: userId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+        return netWorth
+    }catch (error) {
+        console.error('Error fetching networth:', error)
+        throw new Error('Failed to fetch networth')
+    }
+}
